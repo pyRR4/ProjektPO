cb49347fb72d85301737f585ea7ab1695d0f0475
darzen <nivnik004@gmail.com>
2024-12-23 16:32:09 +0100

    Hotfix

--- Mockupy/SendMessageUseCase/scripts/redirect_to_main.js
+++ Mockupy/SendMessageUseCase/scripts/redirect_to_main.js
@@ -1,13 +1,9 @@
-function errorPopup() {
-    window.alert("One or more fields are empty or the data entered is invalid!")
-}
 
 function submitTravel() {
     window.alert("Message sent successfully!")
 }
 
 document.getElementById("submit-message").addEventListener("click", submitTravel)
-document.getElementById("submit-message").addEventListener("contextmenu", errorPopup)
 
 document.getElementById('message-form').addEventListener('submit', function (event) {
     event.preventDefault(); 
